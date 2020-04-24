5531188213c190dd44bec100efcf4d4ca135622a
f1eeman <mr.f1eeman@gmail.com>
2020-04-24 16:59:46 +0600

    add completing task 5

---
+++ bin/brain-even.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { evenCheck } from '../src/index.js';
+
+evenCheck();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+export const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May i have your name? ');
   console.log(`Hello, ${userName}!`);
+};
+
+const getRandomNum = () => Math.floor(Math.random() * 100) + 1;
+
+export const evenCheck = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May i have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no"');
+  let counter = 0;
+
+  for (let i = 0; i < 3; i += 1) {
+    const randomNum = getRandomNum();
+    console.log(`Question: ${randomNum}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const isEven = randomNum % 2 === 0;
+    if ((isEven && (userAnswer === 'yes')) || (!isEven && (userAnswer === 'no'))) {
+      console.log('Correct');
+      counter += 1;
+    } else {
+      const correctAnswer = isEven ? 'yes' : 'no';
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}". \n Let's try again, ${userName}!`);
+      break;
+    }
+  }
+  if (counter === 3) console.log(`Congratulations, ${userName}!`);
 };
