e287d16e60f4096eda6da45a174b6049e90d006b
f1eeman <mr.f1eeman@gmail.com>
2020-04-25 00:09:55 +0600

    add 6 task

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 [![Test Coverage](https://api.codeclimate.com/v1/badges/f7468856ebb70ff0f2a4/test_coverage)](https://codeclimate.com/github/f1eeman/frontend-project-lvl1/test_coverage)
 ![Node CI](https://github.com/f1eeman/frontend-project-lvl1/workflows/Node%20CI/badge.svg)
 
-How to install & how it works?
+How to install & how first game works?
 
 [![asciicast](https://asciinema.org/a/KSjPo4hr7H8P8MY1y1tBtMMxm.svg)](https://asciinema.org/a/KSjPo4hr7H8P8MY1y1tBtMMxm?theme=monokai)
+
+How second game works?
+
+[![asciicast](https://asciinema.org/a/Pr1d4jPPaQAk1FtrelB7rQ6cz.svg)](https://asciinema.org/a/Pr1d4jPPaQAk1FtrelB7rQ6cz?theme=solarized-dark)
--- src/index.js
+++ src/index.js
@@ -31,3 +31,40 @@ export const evenCheck = () => {
   }
   if (counter === 3) console.log(`Congratulations, ${userName}!`);
 };
+
+export const calc = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May i have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('What is the result of the expression?');
+  let counter = 0;
+  const operators = ['+', '-', '*'];
+
+  for (let i = 0; i < 3; i += 1) {
+    const firstNum = getRandomNum();
+    const secondNum = getRandomNum();
+    console.log(`Question: ${firstNum} ${operators[i]} ${secondNum}`);
+    let correctAnswer;
+    switch (operators[i]) {
+      case '+':
+        correctAnswer = firstNum + secondNum;
+        break;
+      case '-':
+        correctAnswer = firstNum - secondNum;
+        break;
+      default:
+        correctAnswer = firstNum * secondNum;
+        break;
+    }
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (Number(userAnswer) === correctAnswer) {
+      console.log('Correct');
+      counter += 1;
+    } else {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}". \n Let's try again, ${userName}!`);
+      break;
+    }
+  }
+  if (counter === 3) console.log(`Congratulations, ${userName}!`);
+};
