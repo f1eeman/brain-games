d07c00d539338e72c4665fdb018b7ec383edfb80
f1eeman <mr.f1eeman@gmail.com>
2020-04-30 15:08:19 +0600

    corrects gcd function

--- src/games/greatest-divisor.js
+++ src/games/greatest-divisor.js
@@ -7,17 +7,11 @@ const getGreatestCommonDivisor = (firstNum, secondNum) => {
   if ((firstNum % smallerNum === 0) && (secondNum % smallerNum === 0)) {
     return smallerNum;
   }
-  let greatestCommonDivisor = 1;
-  const beginValue = Math.floor(smallerNum / 2);
-  for (let i = beginValue; i > 1; i -= 1) {
-    const isNotRemainderOfFirstNum = firstNum % i === 0;
-    const isNotRemainderOfSecondNum = secondNum % i === 0;
-    if (isNotRemainderOfFirstNum && isNotRemainderOfSecondNum) {
-      greatestCommonDivisor = i;
-      break;
-    }
+  let gcd = Math.floor(smallerNum / 2);
+  while (firstNum % gcd !== 0 || secondNum % gcd !== 0) {
+    gcd -= 1;
   }
-  return greatestCommonDivisor;
+  return gcd;
 };
 
 export default () => {
